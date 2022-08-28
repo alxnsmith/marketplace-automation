f648629a7eddcab188a0ac17e3005657f9e687be
nillkizz <alx.n.smith@gmail.com>
2022-08-28 18:40:42 +0300

    Add campaign_id fillment check on get_orders form

--- resources/views/tools/yandex-market/get-orders-form.blade.php
+++ resources/views/tools/yandex-market/get-orders-form.blade.php
@@ -1,21 +1,26 @@
 <x-dashboard-layout title="Заказы Yandex Market" back="true" :backUrl="route('dashboard')">
-  <form action="" method="get">
-    <input type="number" name="campaign_id" placeholder="campaign_id" value="{{ session('YANDEX_CAMPAIGN_ID') }}" required>
-    <select name="status">
-      <option value="">Статус</option>
-      <option value="">Любой Статус</option>
-      <option value="PROCESSING" selected>Обрабатывается</option>
-    </select>
-    <select name="substatus">
-      <option value="">Подстатус</option>
-      <option value="">Любой Подстатус</option>
-      <option value="STARTED" selected>Можно комплектовать</option>
-      {{-- <option value="READY_TO_SHIP">Готов к отгрузке</option> --}}
-    </select>
-    <label>
-      <input type="checkbox" name="fake" checked>
-      Тестовые
-    </label>
-    <x-button name="action">Запросить</x-button>
-  </form>
+  @empty($campaign_id)
+    <div class="alert alert-danger show">
+      Не выбрана кампания, перейдите в <a href="{{ route('dashboard.tools.yandex-market.settings') }}">настройки</a>.
+    </div>
+  @else
+    <form action="" method="get">
+      <select name="status">
+        <option value="">Статус</option>
+        <option value="">Любой Статус</option>
+        <option value="PROCESSING" selected>Обрабатывается</option>
+      </select>
+      <select name="substatus">
+        <option value="">Подстатус</option>
+        <option value="">Любой Подстатус</option>
+        <option value="STARTED" selected>Можно комплектовать</option>
+        {{-- <option value="READY_TO_SHIP">Готов к отгрузке</option> --}}
+      </select>
+      <label>
+        <input type="checkbox" name="fake" checked>
+        Тестовые
+      </label>
+      <x-button name="action">Запросить</x-button>
+    </form>
+  @endempty
 </x-dashboard-layout>
