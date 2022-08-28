627517836aa37134e296cf16a7483fe4af20aa09
nillkizz <alx.n.smith@gmail.com>
2022-08-28 16:39:34 +0300

    Add typehints

--- app/Services/YandexMarketService.php
+++ app/Services/YandexMarketService.php
@@ -8,31 +8,31 @@ use Yandex\OAuth\Exception\AuthRequestException;
 
 class YandexMarketService
 {
-  static function get_client()
+  static function get_client(): OAuthClient
   {
     return new OAuthClient(self::get_client_id(), self::get_client_secret());
   }
 
 
-  static function get_access_token()
+  static function get_access_token(): string
   {
     return session('YANDEX_ACCESS_TOKEN');
   }
-  static function get_client_id()
+  static function get_client_id(): string
   {
     return env('YANDEX_CLIENT_ID');
   }
-  static function get_client_secret()
+  static function get_client_secret(): string
   {
     return env('YANDEX_CLIENT_SECRET');
   }
 
-  static function get_auth_header()
+  static function get_auth_header(): string
   {
     return 'OAuth oauth_token="' . self::get_access_token() . '" , oauth_client_id="' . self::get_client_id() . '"';
   }
 
-  static function get_orders($campaign_id,)
+  static function get_orders($campaign_id): array
   {
     $URL = "https://api.partner.market.yandex.ru/v2/campaigns/{$campaign_id}/orders.json";
     $params = request()->only(['status', 'substatus', 'fake']);
@@ -44,7 +44,7 @@ class YandexMarketService
     return $response;
   }
 
-  static function get_access_token_on_webhook()
+  static function get_access_token_on_webhook(): string
   {
     // Инициализируем OAuth-клиент
     $client = YandexMarketService::get_client();
