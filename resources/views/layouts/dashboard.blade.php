a64f08e01ca5ebe839b1c1bf65d9b1c5918424d8
nillkizz <alx.n.smith@gmail.com>
2022-08-29 20:15:59 +0300

    Remove back button

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,7 +1,7 @@
-@props(['title' => 'Dashboard', 'hasBack' => false, 'backUrl' => url()->previous()])
+@props(['title' => 'Dashboard'])
 
 <x-app-layout>
-  <x-slot name="header">
+  <x-slot name="header" class="relative">
     <h2 class="text-xl font-semibold leading-tight text-gray-800">
       {{ $title }}
     </h2>
@@ -10,13 +10,7 @@
   <div class="py-12">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-        <div @class([
-            'pt-16 relative' => $hasBack,
-            'p-6 bg-white border-b border-gray-200',
-        ])>
-          @if ($hasBack)
-            <x-dashboard.back-btn :backUrl="$backUrl" />
-          @endif
+        <div class="border-b border-gray-200 bg-white p-6">
           {{ $slot }}
         </div>
       </div>
