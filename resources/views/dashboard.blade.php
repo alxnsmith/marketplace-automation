aa3fc421a8dac7ff4c7a104fa2c57d92031b2e35
nillkizz <alx.n.smith@gmail.com>
2022-08-29 20:24:22 +0300

    Refactor navlinks navigation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-dashboard-layout title="Инструменты">
+<x-dashboard-layout title="Панель инструментов">
   @foreach ($tools as $tool_template => $data)
     <div class="card-items grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] gap-4">
       @include($tool_template, $data)
