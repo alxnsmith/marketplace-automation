eb79674c2df947a9f4fa0a23ae7bef4fd4dc6e20
nillkizz <alx.n.smith@gmail.com>
2022-09-09 16:10:38 +0300

    Rework Notifications JS

--- Modules/Core/Resources/views/components/_notifications.blade.php
+++ Modules/Core/Resources/views/components/_notifications.blade.php
@@ -2,67 +2,92 @@
 </div>
 
 <script>
-  window.notifyJs = (content, type = 'success') => {
-    $notify = document.createElement('div');
-    $notify.classList.add('notify');
-    $notify.classList.add(`notify-${type}`);
-
-    $notifyContent = document.createElement('div');
-    $notifyContent.classList.add('notify-content');
-    $notifyContent.innerHTML = content;
-    $notify.appendChild($notifyContent);
-
-    document.getElementById('notifications').appendChild($notify);
-  }
-  window.responseErrorHandler = (e) => {
-    const title = e.message;
-    if (e.response.status === 422) {
-      const message = `<p>${title}</p><ul><li>${e.response.data.message}</li></ul>`;
-      notifyJs(message, 'danger');
-      return;
+  (() => {
+    class TAG {
+      static p = (html) => `<p>${html}</p>`
+      static ul = (html) => `<ul class="ml-4">${html}</ul>`
+      static li = (html) => `<li class="list-disc">${html}</li>`
     }
-    notifyJs(title, 'danger');
-  }
 
-  document.addEventListener('DOMContentLoaded', function() {
-    watchForNotifications(document.getElementById('notifications'));
-    const DELAY_OUT = 5000;
-    const alerts = [];
-    const errors = @json($errors->all('<li class="list-disc">:message</li>'));
-    const notifications = @json($notifications);
+    class Notification {
+      constructor(content, type) {
+        console
+        this.content = content;
+        this.type = type;
+      }
 
-    if (errors.length > 0) {
-      alerts.push({
-        content: `<ul class="ml-4">${errors.join('')}</ul>`,
-        type: 'danger'
-      });
+      get $el() {
+        const $tmpEl = document.createElement('div');
+        $tmpEl.innerHTML = /*html*/ `
+          <div class="notify notify-${this.type}">
+            <div class="notify-content">
+              ${this.content}
+            </div>
+          </div>
+        `;
+        return $tmpEl.firstElementChild;
+      }
+
+      static trigger(content, type = 'successs') {
+        const notification = new Notification(content, type);
+        const $el = notification.$el;
+        document.getElementById('notifications').appendChild($el);
+        return notification;
+      }
     }
-    if (notifications.length > 0) {
-      notifications.forEach(notification => {
+
+    window.notifyJs = Notification.trigger;
+
+    window.responseErrorHandler = (e) => {
+      const title = e.message;
+      if (e.response.status === 422) {
+        const message = TAG.p(title) + TAG.ul(TAG.li(e.response.data.message));
+        notifyJs(message, 'danger');
+        return;
+      }
+      notifyJs(title, 'danger');
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+      watchForNotifications(document.getElementById('notifications'));
+      const DELAY_OUT = 5000;
+      const alerts = [];
+      const errors = @json($errors->all()).map(e => TAG.li(e));
+      const notifications = @json($notifications);
+
+      if (errors.length > 0) {
         alerts.push({
-          content: notification.content,
-          type: notification.type
+          content: TAG.ul(errors.join('')),
+          type: 'danger'
         });
-      });
-    }
-
-
-    alerts.forEach(alert => {
-      window.notifyJs(alert.content, alert.type);
-    });
-
-
-    // Observe for $notifications child nodes, and show them
-    function watchForNotifications($node) {
-      new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          mutation.addedNodes.forEach(($notify) => {
-            setTimeout(() => $notify.remove(), DELAY_OUT);
+      }
+      if (notifications.length > 0) {
+        notifications.forEach(notification => {
+          alerts.push({
+            content: notification.content,
+            type: notification.type
           });
         });
-      }).observe($node, {
-        childList: true
+      }
+
+
+      alerts.forEach(alert => {
+        Notification.trigger(alert.content, alert.type);
       });
-    }
-  });
+
+
+      // Observe for $notifications child nodes, and show them
+      function watchForNotifications($node) {
+        new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach(($notify) => {
+              setTimeout(() => $notify.remove(), DELAY_OUT);
+            });
+          });
+        }).observe($node, {
+          childList: true
+        });
+      }
+    });
+  })()
 </script>
