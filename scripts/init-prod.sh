24146ae8000df877ce7269be2ded74ef259be933
nillkizz <alx.n.smith@gmail.com>
2022-09-18 15:31:23 +0300

    Fix prod init migration

--- scripts/init-prod.sh
+++ scripts/init-prod.sh
@@ -10,10 +10,16 @@ yarn build
 echo " > php artisan key:generate"
 php artisan key:generate
 
-echo " > php artisan migrate:fresh --seed"
-php artisan migrate:fresh --seed
-
 echo " > php artisan storage:link"
 php artisan storage:link
+
+echo " > docker compose up -d"
+docker compose up -d
+
+echo " > docker compose exec laravel.market-automation php artisan migrate:fresh --seed"
+docker compose exec laravel.market-automation php artisan migrate:fresh --seed
+
+echo " > docker compose stop"
+docker compose stop
 
 echo "Done!"
