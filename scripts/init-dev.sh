490a0874db6b038fe1b320b968424fd3c1efc2d9
nillkizz <alx.n.smith@gmail.com>
2022-09-18 13:49:39 +0300

    rework init-dev script

--- scripts/init-dev.sh
+++ scripts/init-dev.sh
@@ -1,8 +1,18 @@
+echo " > composer update"
 composer update
-sail yarn install
+echo " > sail up -d"
+sail up -d
+echo " > sail yarn install"
+sail yarn install --force
 
+echo " > sail artisan key:generate"
 sail artisan key:generate
+echo " > sail artisan migrate:fresh --seed"
 sail artisan migrate:fresh --seed
+echo " > sail artisan storage:link"
 sail artisan storage:link
+
+echo " > sail stop"
+sail stop
 
 echo "Done!"
