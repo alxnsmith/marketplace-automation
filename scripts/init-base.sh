80e3e0310d2ff50673b3e76ae152a55575736440
nillkizz <alx.n.smith@gmail.com>
2022-09-18 15:04:08 +0300

    now env is created on initialization

--- scripts/init-base.sh
+++ scripts/init-base.sh
@@ -1,3 +1,15 @@
+# By default - PROD mode
+# If $0 is --dev - DEV mode
+if [ "$0" = "--dev" ]; then
+  echo "DEV mode"
+  DEV_MODE=true
+  cp .env.example.dev .env
+else
+  echo "PROD mode"
+  DEV_MODE=false
+  cp .env.example.prod .env
+fi
+
 echo "Install composer"
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
